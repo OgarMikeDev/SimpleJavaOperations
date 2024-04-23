35ad5b69fa67656e000041a75a4e9a9b55bd8595
Mike <ogar.m.v@mail.ru>
2024-04-23 11:18:27 +0300

    Letters and numbers

--- src/main/java/MainFour.java
+++ src/main/java/MainFour.java
@@ -13,9 +13,23 @@ public class MainFour {
          */
 
         ArrayList<Integer> listFirst = new ArrayList<>();
+        int countZeros = 0;
+        int countNegativeNumbers = 0;
+        int countPositiveNumbers = 0;
 
         for (int i = 0; i < (int) (10 + Math.round(Math.random() * 10)); i++) {
             listFirst.add((int) (-5 + Math.round(Math.random() * 10)));
+            if (listFirst.get(i) == 0) {
+                countZeros++;
+            }
+
+            if (listFirst.get(i) > 0) {
+                countPositiveNumbers++;
+            }
+
+            if (listFirst.get(i) < 0) {
+                countNegativeNumbers++;
+            }
         }
 
         System.out.print("Fist list: ");
@@ -23,5 +37,8 @@ public class MainFour {
 
         System.out.println("\nMin value: " + Collections.min(listFirst));
         System.out.println("Max value: " + Collections.max(listFirst));
+        System.out.println("Count zeros: " + countZeros);
+        System.out.println("Count positive numbers: " + countPositiveNumbers);
+        System.out.println("Count negative numbers: " + countNegativeNumbers);
     }
 }
