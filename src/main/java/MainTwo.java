4b223604318cc268acb8c48e40494c36f5808506
Mike <ogar.m.v@mail.ru>
2024-04-23 10:11:13 +0300

    Letters and numbers

---
+++ src/main/java/MainTwo.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class MainTwo {
+    public static void main(String[] args) {
+        ArrayList<Integer> listFirst = new ArrayList<>();
+        ArrayList<Integer> listSecond = new ArrayList<>();
+        ArrayList<Integer> listThird = new ArrayList<>();
+
+        for (int i = 0; i < (int) (5 + Math.round(Math.random() * 5)); i++) {
+            listFirst.add((int) (1 + Math.round(Math.random() * 5)));
+            listSecond.add((int) (1 + Math.round(Math.random() * 5)));
+
+            if (listFirst.contains(listSecond.get(i)) &&
+                    !listThird.contains(listSecond.get(i))) {
+                listThird.add(listSecond.get(i));
+            }
+        }
+
+        System.out.print("Fist list: ");
+        listFirst.forEach(System.out::print);
+        System.out.print("\nSecond list: ");
+        listSecond.forEach(System.out::print);
+        System.out.print("\nThird list: ");
+        listThird.forEach(System.out::print);
+    }
+}
